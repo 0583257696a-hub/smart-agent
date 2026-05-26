935cd642849f56d1a42a0d19cfe8586b1ce76bdf
0583257696a-hub <0583257696a-hub@users.noreply.github.com>
2026-05-26 18:08:02 +0300

    revert local launcher nextjs change

--- הפעלת_שרת_מקומי.bat
+++ הפעלת_שרת_מקומי.bat
@@ -1,10 +1,6 @@
 @echo off
 title Agent Operations Server
-echo Starting Next.js server on http://127.0.0.1:3002
-echo.
-echo If port 3002 is busy, close this window and run:
-echo npm.cmd run dev -- --hostname 127.0.0.1 --port 3003
-echo.
-start "" http://127.0.0.1:3002
-npm.cmd run dev -- --hostname 127.0.0.1 --port 3002
+echo Starting server on http://127.0.0.1:5174/index.html
+start http://127.0.0.1:5174/index.html
+node dev-server.js
 pause
