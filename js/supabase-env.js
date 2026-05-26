7a9546905b405bc7082278861023648484e1c5ec
0583257696a-hub <0583257696a-hub@users.noreply.github.com>
2026-05-26 18:08:27 +0300

    revert nextjs app shell migration

--- js/supabase-env.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var current = window.AGENT_OPS_SUPABASE || {};
-  window.AGENT_OPS_SUPABASE = {
-    url: current.url || "__SUPABASE_URL__",
-    publishableKey: current.publishableKey || "__SUPABASE_PUBLISHABLE_KEY__"
-  };
-}());
